fce203b4811465582fcc32f24276e44d99193782
Qoot Alkulaib <qootalkulaib@hotmail.com>
2022-01-07 12:41:42 +0300

    Merge branch 'tahaniALjohni:master' into master

--- app/src/main/java/com/example/tasktimerappgroup4/RVAdapter.kt
+++ app/src/main/java/com/example/tasktimerappgroup4/RVAdapter.kt
@@ -1,24 +1,35 @@
 package com.example.tasktimerappgroup4
 
+import android.app.Dialog
+import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tasktimerappgroup4.Activity.MainActivity
 import com.example.tasktimerappgroup4.Activity.TimeActivity
 import com.example.tasktimerappgroup4.Activity.updateActivity
 import com.example.tasktimerappgroup4.Model.Tasks
 import com.example.tasktimerappgroup4.databinding.ItemRowBinding
+import kotlinx.android.synthetic.main.dialog_builder_add.*
+import kotlinx.android.synthetic.main.dialog_builder_add.etTitle
+import kotlinx.android.synthetic.main.dialog_builder_edit.*
 
-class RVAdapter(private val activity: MainActivity): RecyclerView.Adapter<RVAdapter.ItemViewHolder>() {
+class RVAdapter(private val activity: MainActivity) :
+    RecyclerView.Adapter<RVAdapter.ItemViewHolder>() {
 
-    var tasksList = emptyList<Tasks>()
-    class ItemViewHolder(val binding: ItemRowBinding):RecyclerView.ViewHolder(binding.root)
+    private var tasksList = emptyList<Tasks>()
+    private lateinit var activityContext: Context
 
+    class ItemViewHolder(val binding: ItemRowBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        activityContext = parent.context
         return ItemViewHolder(
-            ItemRowBinding.inflate(LayoutInflater.from(parent.context),parent ,false)
+            ItemRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         )
 
     }
@@ -42,15 +53,27 @@ class RVAdapter(private val activity: MainActivity): RecyclerView.Adapter<RVAdap
             }
 
             //the edit button pressed
-            btnEdit.setOnClickListener{
-                //intent to update activite
-                val intent = Intent(holder.itemView.context, updateActivity::class.java)
-                intent.putExtra("id", task.id)
-                intent.putExtra("title", task.title)
-                intent.putExtra("description", task.description)
-                intent.putExtra("taskTime", task.taskTime)
-                intent.putExtra("isRunning", task.isRunning)
-                holder.itemView.context.startActivity(intent)
+            btnEdit.setOnClickListener {
+                //intent to update activity
+                val dialogBuilder = Dialog(activityContext)
+                dialogBuilder.setContentView(R.layout.dialog_builder_edit)
+                dialogBuilder.window?.setBackgroundDrawableResource(R.drawable.dialog_window)
+
+                var titleE = dialogBuilder.etTitleE.text
+                var detailsE = dialogBuilder.etDetailsE.text
+                val edit = dialogBuilder.btSubmitE
+                //button interaction
+                edit.setOnClickListener {
+                    //TODO:update to database functionality
+                }
+
+                dialogBuilder.show()//                val intent = Intent(holder.itemView.context, updateActivity::class.java)
+//                intent.putExtra("id", task.id)
+//                intent.putExtra("title", task.title)
+//                intent.putExtra("description", task.description)
+//                intent.putExtra("taskTime", task.taskTime)
+//                intent.putExtra("isRunning", task.isRunning)
+//                holder.itemView.context.startActivity(intent)
 
             }
 
@@ -60,9 +83,8 @@ class RVAdapter(private val activity: MainActivity): RecyclerView.Adapter<RVAdap
             }
 
             //setting the total time
-
             val myIntent = Intent(holder.itemView.context, TimeActivity::class.java)
-            val total = myIntent.getLongExtra("total",0)
+            val total = myIntent.getLongExtra("total", 0)
 
             println("here is the total from  getLongExtra $total")
             tvTimeSmall.text = "$total"
@@ -75,9 +97,12 @@ class RVAdapter(private val activity: MainActivity): RecyclerView.Adapter<RVAdap
         return tasksList.size
     }
 
-    fun updateData(notes: List<Tasks>){
+    fun updateData(notes: List<Tasks>) {
         this.tasksList = notes
         notifyDataSetChanged()
     }
 
+    private fun dialogBuild2() {
+
+    }
 }
